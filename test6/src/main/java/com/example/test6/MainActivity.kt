d09ce422f8f36227f5882258c1f768a3d5ddb561
Ryumyuong <fbaudwh@naver.com>
2023-05-11 14:01:34 +0900

    20230511 4교시

--- test6/src/main/java/com/example/test6/MainActivity.kt
+++ test6/src/main/java/com/example/test6/MainActivity.kt
@@ -5,15 +5,31 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import com.example.test6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {//savedInstanceState -> 객체에 저장
         super.onCreate(savedInstanceState)
             //xml 파일을 불러서 화면 출력, R.layout에 저장되어서 불러온다
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        val button1 = findViewById<Button>(R.id.btn1)
+        //뷰 바인딩
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var status = 0
+        binding.btn1.setOnClickListener {
+            if (status == 0) {
+                binding.img1.visibility = View.INVISIBLE
+                status = 1
+            }else {
+                binding.img1.visibility = View.VISIBLE
+                status = 0
+            }
+        }
+
+        /*val button1 = findViewById<Button>(R.id.btn1)
         val img1 = findViewById<ImageView>(R.id.img1)
         var status = 0
 
@@ -25,6 +41,6 @@ class MainActivity : AppCompatActivity() {
                 img1.visibility = View.VISIBLE
                 status = 0
             }
-        }
+        }*/
     }
 }
