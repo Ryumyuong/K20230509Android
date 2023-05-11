bf96b3b35e1d7dcff38882f1e5343147dc3a941b
Ryumyuong <fbaudwh@naver.com>
2023-05-11 11:57:58 +0900

    20230511 3교시

--- test6/src/main/java/com/example/test6/MainActivity.kt
+++ test6/src/main/java/com/example/test6/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.test6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
             //xml 파일을 불러서 화면 출력, R.layout에 저장되어서 불러온다
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.btn1)
+        val img1 = findViewById<ImageView>(R.id.img1)
+        var status = 0
+
+        button1.setOnClickListener {
+            if (status == 0) {
+                img1.visibility = View.INVISIBLE
+                status = 1
+            }else {
+                img1.visibility = View.VISIBLE
+                status = 0
+            }
+        }
     }
 }
