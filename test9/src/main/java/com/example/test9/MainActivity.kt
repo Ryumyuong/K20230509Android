68699c2eabf006d4917105825fa15d4c4f6f2fd1
Ryumyuong <fbaudwh@naver.com>
2023-05-12 15:13:59 +0900

    20230512 5교시

--- test9/src/main/java/com/example/test9/MainActivity.kt
+++ test9/src/main/java/com/example/test9/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.test9
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.WindowMetrics
 import com.example.test9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +14,18 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //values의 strings.xml에서 가져오기
-//        binding.textView1.text = getString(R.string.test)
+//      binding.textView1.text = getString(R.string.test)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics : WindowMetrics = windowManager.currentWindowMetrics
+            Log.d("lmj", "width: ${windowMetrics.bounds.width()}, height: ${windowMetrics.bounds.height()}")
+        } else {
+            val display = windowManager.defaultDisplay
+            val displayMetrics = DisplayMetrics()
+            display?.getRealMetrics(displayMetrics)
+            Log.d("lmj", "width: ${displayMetrics.widthPixels}, height: ${displayMetrics.heightPixels}")
+        }
     }
+
+
 }
