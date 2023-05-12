e82203b07f2645baa19c8dc15fa54801a1a42d7e
Ryumyuong <fbaudwh@naver.com>
2023-05-12 11:50:47 +0900

    20230512 3교시

--- test9/src/main/java/com/example/test9/MainActivity.kt
+++ test9/src/main/java/com/example/test9/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.test9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.test9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //values의 strings.xml에서 가져오기
+        binding.textView1.text = getString(R.string.test)
     }
 }
