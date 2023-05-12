52aa5b31a5ccbad8b4d70a01957e041477a01c27
Ryumyuong <fbaudwh@naver.com>
2023-05-12 16:41:41 +0900

    20230512 7교시

--- test9/src/main/java/com/example/test9/MainActivity2.kt
+++ test9/src/main/java/com/example/test9/MainActivity2.kt
@@ -2,10 +2,17 @@ package com.example.test9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.test9.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.button.isEnabled = !binding.button.isEnabled
+        }
+
     }
 }
