2bb1fb2bf77ca31ad5a5a3c12d7e61e20b301484
Ryumyuong <fbaudwh@naver.com>
2023-05-15 09:49:28 +0900

    20230515 1교시

--- test10/src/main/java/com/example/test10/MainActivity.kt
+++ test10/src/main/java/com/example/test10/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.test10
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.test10.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +16,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /*결과값을 엑티비티에 등록
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                Log.d("lmj", "승인됨")
+            } else {
+                Log.d("lmj", "승인 안됨")
+            }
+        }
+
+        val status = ContextCompat.checkSelfPermission(this,"android.permission.ACCESS_FINE_LOCATION")
+
+        if(status == PackageManager.PERMISSION_GRANTED) {
+            Log.d("lmj", "승인됨2")
+        } else {
+            requestPermissionLauncher.launch("android.permission.ACCESS_FINE_LOCATION")
+        }*/
+
+        val toast = Toast.makeText(this,"메세지 내용",Toast.LENGTH_SHORT)
+        binding.btn1.setOnClickListener {
+            toast.show()
+
+            Toast.makeText(this, "토스트 출력 방법2", Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
