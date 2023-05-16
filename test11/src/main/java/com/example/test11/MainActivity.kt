5c400b73932ee5fc4f18b471f16b8246bb18b459
Ryumyuong <fbaudwh@naver.com>
2023-05-16 13:09:57 +0900

    20230516 4교시

--- test11/src/main/java/com/example/test11/MainActivity.kt
+++ test11/src/main/java/com/example/test11/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.example.test11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.test11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /*override fun onSupportNavigateUp(): Boolean {
+        Toast.makeText(this@MainActivity, "메인 업버튼 동작",Toast.LENGTH_SHORT).show()
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }*/
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuItem1 : MenuItem? = menu?.add(0,0,0,"메뉴1")
+        val menuItem2 : MenuItem? = menu?.add(0,1,0,"메뉴2")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
+        0 -> {
+            Toast.makeText(this@MainActivity, "메뉴 1", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        1 -> {
+            val intent = Intent(this@MainActivity, ThreeActivity::class.java)
+            startActivity(intent)
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //화면 전환
+        binding.btn1.setOnClickListener {
+            val intent = Intent(this@MainActivity, TwoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ test11/src/main/java/com/example/test11/MainActivity3.kt
@@ -0,0 +1,19 @@
+package com.example.test11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = OneFragment()
+        transaction.add(R.id.fragment_content, fragment)
+        transaction.commit()
+    }
+}
