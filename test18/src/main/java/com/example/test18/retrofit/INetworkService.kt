3e631e335f24921dc5ca2b205edd7c3606e53d3c
Ryumyuong <fbaudwh@naver.com>
2023-05-23 12:12:51 +0900

    202305123 4교시

---
+++ test18/src/main/java/com/example/test18/retrofit/INetworkService.kt
@@ -0,0 +1,21 @@
+package com.example.test18.retrofit
+
+import com.example.test18.Model.UserListModel
+import com.example.test18.Model.UserModel
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface INetworkService {
+    @GET("api/users")
+//    https://reqres.in/api/users?page=2 와 동일
+    fun doGetUserList(@Query("page") page: String): Call<UserListModel>
+    @GET
+    fun getAvatarImage(@Url url: String): Call<ResponseBody>
+
+//    @GET("users/list?sort=desc")
+    @GET("api/users/2")
+    fun test1(): Call<UserModel>
+}
