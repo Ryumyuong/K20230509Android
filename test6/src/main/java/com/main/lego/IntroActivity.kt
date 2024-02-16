f7fda67f1cfe82040ff56e3767d83927e0a0aa52
Ryu <fbaudwh@naver.com>
2024-02-16 14:49:00 +0900

    인트로와 버튼 색깔변경

---
+++ test6/src/main/java/com/main/lego/IntroActivity.kt
@@ -0,0 +1,27 @@
+package com.main.lego
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.main.lego.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    lateinit var binding: ActivityIntroBinding
+    private val SPLASH_TIMEOUT:Long = 2000 //2초
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this@IntroActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIMEOUT)
+
+    }
+
+
+}
